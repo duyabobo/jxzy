b69cbab54ba0b4de4de68cb4ed2de5bf8981937d
duyabo <duyabo@hundun.cn>
2025-10-29 23:46:20 +0800

    Remove Bailian API configuration from bllcontext.yaml and related struct from config.go to streamline configuration management.

--- bll/bll_context/internal/config/config.go
+++ bll/bll_context/internal/config/config.go
@@ -8,13 +8,8 @@ type Config struct {
 	LLMRpc   zrpc.RpcClientConf `json:",optional"`
 	BsLlmRpc zrpc.RpcClientConf `json:",optional"`
 	BsRagRpc zrpc.RpcClientConf `json:",optional"`
-	Bailian  BailianConfig      `json:",optional"`
 }
 
 type MysqlConf struct {
 	DataSource string
-}
-
-type BailianConfig struct {
-	APIKey string `json:"api_key"`
 }
